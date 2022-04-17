793e1897e79699e3692e3d13c9e91b8f796dba22
imhyojin <1004dlagywls@naver.com>
2022-04-17 23:54:57 +0900

    HikariPool

--- src/main/java/com/example/with/member/MemberController.java
+++ src/main/java/com/example/with/member/MemberController.java
@@ -1,26 +1,19 @@
 package com.example.with.member;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import com.example.with.product.Product;
+import com.example.with.product.ProductService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.with.product.Product;
-import com.example.with.product.ProductService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 
 @Controller // 컨트롤러 지정
 public class MemberController {
